be7f759064ab9ffc399eb10911e2ccde9ad54324
Mohammed <mohammedmiessal@gmail.com>
2024-02-25 17:52:24 +0100

    Model Role with his CRUD is completed

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::group([], function () {
 Route::prefix('role')->group(function () {
     Route::get('/', [RoleController::class, 'index'])->name('role.index');
     Route::get('/index', [RoleController::class, 'index'])->name('role.index');
+    Route::get('/create', [RoleController::class, 'create'])->name('role.create');
     Route::post('/store', [RoleController::class, 'store'])->name('role.store');
     Route::get('edit/{role}', [RoleController::class, 'edit'])->name('role.edit');
     Route::put('/update/{role}', [RoleController::class, 'update'])->name('role.update');
